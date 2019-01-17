06153f35b66fe34526556c336ff20a829e35d03a
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-01-17 16:23:43 +0530

    Adds mock for state-reducer editor. Created a new repo for state-reducer-viewer, not for another state-reducer-editoer. Justification. This was quicker to create, and looks simple

--- src/index.js
+++ src/index.js
@@ -3,15 +3,49 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
+import style from "./index.css";
+
 class Index extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: "Default",
+            data: {
+                status: "Anonymouse"
+            },
+            reducers: [],
+            inProgressReducer: ""
+        }
+    }
+
+    storeReducer(e) {
+        this.setState({
+            inProgressReducer: e.target.value
+        })
+    }
+
+    saveReducer(){
+        let reducer = this.state.inProgressReducer;
+        let reducers = Array.from(this.state.reducers);
+        reducers.push(reducer);
+        this.setState({
+            reducers
+        });
     }
 
     render(){
+        let reducers = this.state.reducers.map(reducer=> <input className={style.smallWidth} disabled value={reducer}/>);
+
         return (
-            <div>
-                <h3>State reducer viewer</h3>
+            <div className={style.box}>
+                <label>Name</label>
+                <input className={style.smallWidth} type="text"/>
+                <label>Data</label>
+                <textarea className={style.smallWidth} />
+                <label>Reducers</label>
+                {reducers}
+                <input placeholder="New reducers" onChange={this.storeReducer.bind(this)} className={style.smallWidth} type="text"/>
+                <button onClick={this.saveReducer.bind(this)}>Add</button>
             </div>
         );
     }
